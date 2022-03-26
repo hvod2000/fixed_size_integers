89f8223b11df4d2f394bcfecbbe04a716436ded0
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 21:07:46 +0300

    implement setting value to fixed size integer

--- fixed_integers.py
+++ fixed_integers.py
@@ -10,3 +10,12 @@ class Int:
             self.bits = args[0]
         else:
             raise Exception()
+
+    @property
+    def value(self):
+        return sum(b.value * 2**p for p, b in enumerate(self.bits))
+
+    @value.setter
+    def set_value(self, value):
+        for i, bit in enumerate(self.bits):
+            bit.value = (value >> i) & 1
