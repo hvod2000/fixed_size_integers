b5e9449ce16e4d5311301f4b8da5f8927ad234be
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 22:49:56 +0300

    implement reparenting

--- numberizator.py
+++ numberizator.py
@@ -1,3 +1,9 @@
+def add_parent(cls, parent):
+    namespace = {"cls": cls, "parent": parent}
+    exec(f"class {cls.__name__}(cls, parent): pass", namespace)
+    return namespace[cls.__name__]
+
+
 def add_method(cls, name, body):
     namespace = {"cls": cls, "name": name, "body": body}
     exec(f"class {cls.__name__}(cls): {name} = body", namespace)
