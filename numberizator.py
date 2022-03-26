2f678897a00065231477bf23bb6f65249d0940db
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 22:58:58 +0300

    refactor numberizator code into separate functions

--- numberizator.py
+++ numberizator.py
@@ -1,4 +1,4 @@
-from numbers import Integral
+from numbers import Integral, Number
 
 
 def add_parent(cls, parent):
@@ -13,21 +13,57 @@ def add_method(cls, name, body):
     return namespace[cls.__name__]
 
 
-def numberizate(original):
-    assert "__int__" in original.__dict__
-    cls = add_parent(original, Integral)
-    for method in (
-        "rpow rrshift add and floordiv invert mod mul neg pos".split()
-        + "lshift pow radd rand rfloordiv rlshift rmod rmul rshift".split()
-        + "truediv rtruediv xor rxor or ror eq le lt".split()
-    ):
-        method = f"__{method}__"
-        if method not in original.__dict__:
-            cls = add_method(cls, method, lambda *args: NotImplemented)
-    for method in "trunc ceil floor round".split():
-        method = f"__{method}__"
-        if method not in original.__dict__:
-            cls = add_method(cls, method, lambda x: int(x))
-    if "__abs__" not in original.__dict__:
-        cls = add_method(cls, "__abs__", lambda x: abs(int(x)))
-    return cls
+def implement_binary_op(binary_operation):
+    namespace, definition = {"Number": Number}, (
+        f"""def {binary_operation}(self, other):
+    if isinstance(other, Number):
+        return int(self).{binary_operation}(other)
+    return NotImplemented"""
+    )
+    exec(definition, namespace)
+    return namespace[binary_operation]
+
+
+def implement_unary_op(op):
+    namespace, definition = {}, f"def {op}(self):\n    return int(self).{op}()"
+    exec(definition, namespace)
+    return namespace[op]
+
+
+def has_method(cls, method):
+    for cls in cls.mro():
+        if cls is object:
+            continue
+        if method in cls.__dict__:
+            return True
+    return False
+
+
+binary_operations = list(
+    sorted(
+        "add radd sub rsub mul rmul truediv rtruediv floordiv rfloordiv".split()
+        + "and rand mod rmod lshift rlshift pow rpow rshift rrshift".split()
+        + "or ror xor rxor eq rer le rle lt rlt".split()
+    )
+)
+
+unary_operations = list(
+    sorted("invert neg trunc ceil floor round pos abs".split())
+)
+
+
+def numberizate(cls=None):
+    assert "__int__" in cls.__dict__
+    for method in (f"__{op}__" for op in binary_operations):
+        if not has_method(cls, method):
+            print("add", method)
+            cls = add_method(cls, method, implement_binary_op(method))
+        else:
+            print("DUPLICATE", method)
+    for method in (f"__{op}__" for op in unary_operations):
+        if not has_method(cls, method):
+            print("add", method)
+            cls = add_method(cls, method, implement_unary_op(method))
+        else:
+            print("DUPLICATE", method)
+    return add_parent(cls, Integral)
